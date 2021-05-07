2062790f01ab08d5ce1a0bd8992fbf65c19d5e8e
hassehulabeck <hansbudandersson@gmail.com>
2021-05-07 15:36:42 +0200

    Fixade crud

--- users/index.js
+++ users/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const { update } = require('./models/user')
 const User = require('./models/user')
 
 // För att kunna ta emot post-requests och läsa req.body etc
@@ -21,6 +22,16 @@ db.once('open', () => {
         res.json(result)
     })
 
+    app.get('/:userid', async(req, res) => {
+
+        // en variant isf _id
+        // Ändra då parametern till username
+        //const result = await User.findOne({ firstName: req.params.username })
+
+        const result = await User.findById(req.params.userid)
+        res.json(result)
+    })
+
     app.post('/', async(req, res) => {
         if (req.body.firstName !== null) {
             const newUser = new User({
@@ -30,11 +41,37 @@ db.once('open', () => {
             })
             const result = await newUser.save()
             console.log(result)
+                // För att testa våran metod.
+            console.log(newUser.fullName())
             if (result !== null) {
                 res.json(newUser)
             } else {
                 res.send("Det blev problem med att spara i db.")
             }
+        }
+    })
+
+    app.put('/:userid', async(req, res) => {
+
+        // Loopa igenom req.body för att skapa ett objekt som innehåller de värden som ska ändras. 
+        let updateObject = {}
+        for (property in req.body) {
+            updateObject[property] = req.body[property]
+        }
+
+        // findByAndUpdate behöver ett filter, ett objekt med ändringar och ev. ett options-objekt. 
+        const result = await User.findByIdAndUpdate(
+            req.params.userid,
+            updateObject, { useFindAndModify: false })
+        res.json(result)
+    })
+
+    app.delete('/:userid', async(req, res) => {
+        const result = await User.deleteOne({ _id: req.params.userid })
+        if (result.deletedCount == 1) {
+            res.send("Användare " + req.params.userid + " är raderad.")
+        } else {
+            res.send("Problem uppstod.")
         }
     })
 
